f1feb00a836824262c4716efbcb5cd3e62e4559b
sulaiman_1 <121033451+sulaiman50@users.noreply.github.com>
2023-06-12 00:21:09 +0300

    Update APIUSERController.cs

--- DTO/Controllers/API/APIUSERController.cs
+++ DTO/Controllers/API/APIUSERController.cs
@@ -1,4 +1,4 @@
-﻿using DTO.DTO;
+using DTO.DTO;
 using DTO.Mapconn;
 using DTO.Models;
 using System;
@@ -14,34 +14,54 @@ namespace DTO.Controllers.API
     public class APIUSERController : ApiController
     {
          MoviesDBEntities db = new MoviesDBEntities();
-        private MapProile pro = new MapProile(); 
+        private MapProile pro = new MapProile();
 
-        //[ResponseType(typeof(userDTO))]
-        //public IHttpActionResult GetUser(int id )
-        //{
-        //    var userR = db.users.Find(id);
-
-        //    if(userR == null )
-        //    {
-        //        return NotFound();
-        //    }
-        //    var map = pro.GetMapper();
-        //    var result = map.Map<users,user_DTO>(userR);
-
-        //    return Ok(result);
-        //}
+        // --------------- Get by Id 
 
         [ResponseType(typeof(userDTO))]
-        public IEnumerable<userDTO> GetUsers() 
-        { 
-        
-            var usersL = db.users.ToList();
+        public IHttpActionResult GetUser(int id) // Get By Id
+        {
+            var userR = db.users.Find(id);
+
+            if (userR == null)
+            {
+                return NotFound();
+            }
             var map = pro.GetMapper();
-            var result = map.Map<List<users>, List<userDTO>>(usersL);
-            return result;
+            var result = map.Map<users, userDTO>(userR);
 
-
+            return Ok(result);
         }
+
+        // --------------- Get List Of Users
+
+        //[ResponseType(typeof(userDTO))]
+        //public IEnumerable<userDTO> GetUsers() 
+        //{ 
+        //    var usersL = db.users.ToList();
+        //    var map = pro.GetMapper();
+        //    var result = map.Map<List<users>, List<userDTO>>(usersL);
+        //    return result;
+        //}
+
+
+        // --------------- Post By Id 
+
+        //[ ResponseType(typeof(userDTO))]
+        // [HttpPost]
+        // public IHttpActionResult PostUsersDTO(userDTO userDTO)
+        // {
+        //     var map = pro.GetMapper();
+        //     var result1 = map.Map<userDTO, users>(userDTO);
+        //     var user = db.users.Where(x => x.Id == result1.Id).FirstOrDefault();
+        //     if (user == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     var result = map.Map<users, userDTO>(user);
+
+        //     return Ok(result);
+        // }
 
 
     }
